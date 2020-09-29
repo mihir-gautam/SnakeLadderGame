af98db1095cb569901996d4213cca339c5426311
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 13:09:11 +0530

    [Mihir] Add Game Start with player 1 at score 0

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace SnakeLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snake and ladder program");
+            int ScoreP1 = 0;
+            Console.WriteLine("Game Started, Player 1 is at :" + ScoreP1);
         }
     }
 }
