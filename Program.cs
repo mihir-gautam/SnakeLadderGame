72fa58aaea6bc78a8268cda5c2461cddf1276ec6
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 13:15:38 +0530

    [Mihir] Added number after rolling the die

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace SnakeLadderProblem
             Console.WriteLine("Welcome to snake and ladder program");
             int ScoreP1 = 0;
             Console.WriteLine("Game Started, Player 1 is at :" + ScoreP1);
+            
+            static int DieRoll()
+            {
+                Random random = new Random();
+                int dieNum = random.Next(1, 7);
+                return dieNum;
+            }
+            ScoreP1 = DieRoll();
+            Console.WriteLine("Player score after rolling the die : " + ScoreP1);
         }
     }
 }
