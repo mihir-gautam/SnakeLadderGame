09b0e9697d4da196310ddd4c0c1d0fd3f4400125
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 13:53:09 +0530

    [Mihir] Added check for playing options

--- Program.cs
+++ Program.cs
@@ -6,18 +6,51 @@ namespace SnakeLadderProblem
     {
         static void Main(string[] args)
         {
+            const int NO_PLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
+            const int START = 0;
+            const int END = 100;
+
             Console.WriteLine("Welcome to snake and ladder program");
             int ScoreP1 = 0;
             Console.WriteLine("Game Started, Player 1 is at :" + ScoreP1);
-            
+            int dieNumber = DieRoll();
+            Console.WriteLine("Number on the Die : " + dieNumber);
+            ScoreP1 = nextMove(dieNumber, ScoreP1);
+            Console.WriteLine("New Score : " + ScoreP1);
+
             static int DieRoll()
             {
                 Random random = new Random();
                 int dieNum = random.Next(1, 7);
                 return dieNum;
             }
-            ScoreP1 = DieRoll();
+        
             Console.WriteLine("Player score after rolling the die : " + ScoreP1);
+
+            static int nextMove(int DieNumber, int ScoreP1)
+            {
+                Random random = new Random();
+                int options = random.Next(0, 3);
+                switch (options)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
+                    case SNAKE:
+                        Console.WriteLine("Snake");
+                        ScoreP1 -= DieNumber;
+                        break;
+                    case LADDER:
+                        Console.WriteLine("Ladder chosen");
+                        ScoreP1 += DieNumber;
+                        break;
+                }
+                return ScoreP1;
+                }
+            }
         }
     }
-}
+
+
